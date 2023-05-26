5c10fafe3b55d2fdb5730036a93ac4f6ea20c560
jamesschuler <jameshschuler1@gmail.com>
2023-05-25 21:31:28 -0700

    Added error logging

--- api/src/middleware/handleError.ts
+++ api/src/middleware/handleError.ts
@@ -1,9 +1,12 @@
+import { logError } from "@/handlers/commands/createLog.handler.ts";
 import { RouterContext } from "oak";
 
 export async function handleError(ctx: RouterContext<string>, next: () => Promise<void>) {
   try {
     await next();
-  } catch (_err) {
+  } catch (err) {
+    await logError("Application Error", err);
+
     ctx.response.status = 500;
     ctx.response.body = { message: "Something went wrong processing your request. Please try again later." };
   }
